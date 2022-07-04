70120f1784fec743135031a26e909ce812781201
woong <osw0124@daum.net>
2022-07-04 16:16:01 +0900

    [추가] prettierrc 설정 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,24 +7,24 @@ import 'dotenv/config';
 console.log(process.env);
 
 @Module({
-  imports: [
-    ////////GraphQL
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: 'src/commons/graphql/schema.gql',
-      context: ({ req, res }) => ({ req, res }),
-    }),
-    ///////MySQL
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: 3306,
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASS,
-      database: process.env.MYSQL_DATABASE,
-      entities: [__dirname + '/apis/**/*.entity.*'],
-      logging: true,
-    }),
-  ],
+    imports: [
+        ////////GraphQL
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: 'src/commons/graphql/schema.gql',
+            context: ({ req, res }) => ({ req, res }),
+        }),
+        ///////MySQL
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.MYSQL_HOST,
+            port: 3306,
+            username: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASS,
+            database: process.env.MYSQL_DATABASE,
+            entities: [__dirname + '/apis/**/*.entity.*'],
+            logging: true,
+        }),
+    ],
 })
 export class AppModule {}
