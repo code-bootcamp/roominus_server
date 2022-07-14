67bf7488a13d76b4f4d551fe5645485cffd2730b
woong <osw0124@daum.net>
2022-07-14 19:40:17 +0900

    [추가]feature#30 #61 결제/예약 환불 기능 추가

--- backend/src/apis/payment/payment.service.ts
+++ backend/src/apis/payment/payment.service.ts
@@ -13,6 +13,10 @@ export class PaymentService {
     constructor(
         @InjectRepository(Payment)
         private readonly paymentRepository: Repository<Payment>,
+        @InjectRepository(Reservation)
+        private readonly reservationRepository: Repository<Reservation>,
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
         private readonly iamportService: IamportService,
         private readonly connection: Connection,
     ) {}
@@ -76,6 +80,65 @@ export class PaymentService {
         } catch (error) {
             await queryRunner.rollbackTransaction();
             throw new ConflictException(error, '결제 실패!!');
+        } finally {
+            await queryRunner.release();
+        }
+    }
+
+    async cancel({ reservationId, userId, merchantUid }) {
+        const queryRunner = await this.connection.createQueryRunner();
+        await queryRunner.connect();
+
+        const reservationInfo = await this.reservationRepository.findOne({ id: reservationId });
+        if (reservationInfo.status === '환불완료') throw new UnprocessableEntityException('이미 환불된 예약입니다!!');
+
+        const impAccessToken = (await this.iamportService.getToken()).data.response.access_token;
+        if (!impAccessToken) throw new UnprocessableEntityException('엑세스 토큰 수령 실패!!');
+
+        const payments = await this.paymentRepository.find({
+            where: { merchant_uid: merchantUid },
+            order: { createdAt: 'DESC' },
+        });
+        const { imp_uid, merchant_uid, price, usepoint } = payments[0];
+
+        await this.iamportService.paymentCancel({
+            impAccessToken,
+            imp_uid,
+        });
+
+        await queryRunner.startTransaction();
+
+        try {
+            const newPayment = this.paymentRepository.create({
+                imp_uid,
+                merchant_uid,
+                price: -price,
+                usepoint: -usepoint,
+                user: userId,
+                reservation: reservationId,
+            });
+
+            queryRunner.manager.update(
+                Reservation, //
+                { id: reservationId },
+                { status: '환불완료' },
+            );
+
+            const userInfo = await this.userRepository.findOne({ id: userId });
+            queryRunner.manager.update(
+                User, //
+                { id: userId },
+                { point: userInfo.point - price * 0.03 },
+            );
+
+            queryRunner.manager.save(newPayment);
+
+            await queryRunner.commitTransaction();
+
+            return true;
+        } catch (error) {
+            await queryRunner.rollbackTransaction();
+            throw new UnprocessableEntityException(error, '환불 실패!!');
         } finally {
             await queryRunner.release();
         }
