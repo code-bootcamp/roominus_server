02d1aeded685164b6f3ac01c156cdcb1d8a4bb35
woong <osw0124@daum.net>
2022-07-25 01:03:09 +0900

    [수정]헤더도 cors origin 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: ['http://localhost:3000', 'https://roominus.site'],
+        origin: [
+            'http://localhost:3000', //
+            'https://roominus.site',
+        ],
         optionsSuccessStatus: 204,
         credentials: true,
         exposedHeaders: ['Authorization', 'Set-Cookie', 'Cookie'],
